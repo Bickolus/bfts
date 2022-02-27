0384fbaeff6073211f9f8379b33b35695c060e65
Bilal Raza <smbraza97@gmail.com>
2022-02-28 01:52:37 +0500

    finished adding recycled code with original comments

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Import sequelize and dotenv
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+// Declare sequelize first
+let sequelize;
+
+// Then create a connection to the database, using a .env file (to store values) to log in the mysql server
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
