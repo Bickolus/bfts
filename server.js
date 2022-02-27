6512f9c734717c16eedd3c1c4273d531c4c69b36
Bilal Raza <smbraza97@gmail.com>
2022-02-28 01:49:34 +0500

    added recycled server.js code from lessons with comments

---
+++ server.js
@@ -0,0 +1,33 @@
+// Assignments for libraries and being able to use files/methods/helpers from other folders
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// This creates a session instance with a unique ID
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// Enable connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
