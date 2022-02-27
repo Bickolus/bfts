d09bbaf3832ce7434366b32f9bcef657c908a451
Bilal Raza <smbraza97@gmail.com>
2022-02-28 01:48:47 +0500

    added recycled logout code from lessons and added comments to them

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+// Logout event handler:
+const logout = async () => {
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Failed to log out.");
+  }
+};
+
+// Adding event listener: 
+document.querySelector("#logout").addEventListener("click", logout);
